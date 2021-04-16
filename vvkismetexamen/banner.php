c4652153e56dd7a393a30f5eab613586abead9e8
Lemi <Lemi@DESKTOP-SDD69VQ>
2021-04-16 14:18:31 +0430

    Header Styling

--- vvkismetexamen/banner.php
+++ vvkismetexamen/banner.php
@@ -1,9 +1,8 @@
 <banner>
-<div class="">
-<img src="pic/kismetlogo.jpg" alt="kismetlogo" width="auto" height="200">
-</div>
+  <a href="/" class="logo">
+    <img src="pic/kismetlogo.jpg" alt="kismetlogo" width="auto" height="200">
+  </a>
   <div class="eenblik">
-<img src="pic/c7d6aa0dd51e747092c58378f9499f3e.jpg" alt="club" height="200">
-</div>
-    
+    <div class="fade-gradient"></div>
+  </div>
 </banner>
