fa29fd0cc6e65d704b69bdc5f8247f8d2c339575
Nuha Camara <321592@student.mboutrecht.nl>
2021-10-26 16:05:55 +0200

    lol
    lol

--- archief.php
+++ archief.php
@@ -1,1 +1,7 @@
-work in progress
+<img alt="gallery/test4" src="https://www.vvkismet.net/gallery_gen/1f0923ddd1553ccd5f8cb5b81741c744_1540x680_0x140_1560x950.jpeg">
+<br>
+<img alt="gallery/test4" src="https://www.vvkismet.net/gallery_gen/34a7678480c6d65282392d1de4bbcfd7_1540x680.jpeg">
+<br>
+<img alt="gallery/test4" src="https://www.vvkismet.net/gallery_gen/0f25922d7cc0acf06a5430d71140b7fe_1540x680.jpeg">
+<br>
+<img alt="gallery/test4" src="https://www.vvkismet.net/gallery_gen/d05ee8ed74cd0370ca4fbd0187827a3c_1540x680.jpeg">
--- wedstrijden.php
+++ wedstrijden.php
@@ -1,1 +1,104 @@
-work in progress
+Compititie 4e
+klasse - F
+ <br>
+Zondag 1 December 2019 14:30
+<br>
+<br>
+<br>
+> Kismet 1 VS 't Goy 1
+<br>
+<br> 
+<br>
+Zondag 15 December 2019 14:30
+<br>
+<br> 
+<br>
+> VV De Dreef 1 VS Kismet 1 
+<br>
+<br> 
+<br>
+Zondag 26 Januari 2020 14:00
+<br>
+<br> 
+<br>
+> Waterwijk asc 1 VS Kismet 1
+<br>
+<br> 
+<br>
+Zondag 2 Februari 2020 14:30
+<br>
+<br>
+<br>
+> VVU Ardahanspor 1 VS Kismet 1
+<br>
+<br>
+<br>
+Zondag 9 Februari 2020 14:30
+<br>
+<br>
+<br>
+> Kismet 1 VS Schalkwijk 1 
+<br>
+<br>
+<br>
+Zondag 8 Maart 14:30
+<br>
+<br>
+<br>
+> Kismet 1 VS VV De Dreef 1 
+<br>
+<br>
+<br>
+Zondag 15 Maart 14:00
+<br>
+<br>
+<br>
+>Geinburgia sv. 1 VS Kismet 1
+<br>
+<br>
+<br>
+Zondag 29 Maart 14:30
+<br>
+<br>
+<br>
+> Kismet 1 VS Rivierwijkers 1 
+<br>
+<br>
+<br>
+Zondag  5 April 14:00
+<br>
+<br> 
+<br>
+> 't Goy 1 VS Kismet 1 
+<br>
+<br>
+<br>
+Zondag 19 April 14:00
+<br>
+<br>
+<br>
+> DWSV 1 VS Kismet 1 
+<br>
+<br>
+<br>
+Zondag 10 Mei 14:30
+<br>
+<br>
+<br>
+> Kismet 1 Sporting Almere 1 
+<br>
+<br>
+<br>
+Zondag 17 Mei 14:00
+<br>
+<br>
+<br>
+> Aurora 1 VS Kismet 1 
+<br>
+<br>
+<br>
+Zondag 24 Mei 14:00
+<br>
+<br>
+<br>
+> Kismet 1 VS Amsvorde 1
