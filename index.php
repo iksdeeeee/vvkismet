8c1d4f82ede0a01ac9ede07c267049aa5ff8f308
iksdeeeee <47817973+iksdeeeee@users.noreply.github.com>
2022-01-27 01:53:01 +0100

    yesir

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <head>
       <title>vvkismet</title>
       <link rel="icon" href="/pic/kismetlogo.jpg" type="image/gif">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
       <?php
       include("conect_db.php");
       include("links.phtml");
@@ -22,6 +25,7 @@
       include("banner.php");
       include("navbar.php");
       ?>
+      
       <div id="login" class="logindialog">
 	<div>
 		<a href="#close" title="Close" class="close">X</a>
--- navbar.php
+++ navbar.php
@@ -4,6 +4,7 @@
     <a href="?pageid=contact">contact</a>
     <a href="?pageid=archief">archief</a>
     <a href="?pageid=wedstrijden">wedstrijden</a>
+    <a href="?pageid=registerform">registreren</a>
     <?PHP
         $loggedIn = isset($_SESSION["userid"]);
         if($loggedIn) :
